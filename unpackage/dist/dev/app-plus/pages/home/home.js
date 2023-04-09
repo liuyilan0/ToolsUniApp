5ed85da54b20962d1b1b197c75af54184477db5b
LENOVO\刘怡兰 <liuyilan>
2023-04-09 15:18:27 +0800

    fix: dist删除

--- unpackage/dist/dev/app-plus/pages/home/home.js
+++
@@ -1,2 +0,0 @@
-setCssToHead([".",[1],"status_bar.",[1],"data-v-160debbe { height: var(--status-bar-height); width: 100%; }\n",],undefined,{path:"./pages/home/home.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/home/home.wxml') } }));
